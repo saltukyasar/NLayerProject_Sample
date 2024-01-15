665cf313e101cf6c73078e71e4bc80633201a31d
Yasar <s.yasar@uzuner-consulting.com>
2024-01-15 13:48:59 +0100

    Configurations are created in Repository layer

---
+++ NLayer.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+
+            builder.ToTable("Products");
+
+            // Foregin Key
+            // Bir kategorinin birden fazla producti olabilir. 
+            // Ozellikle Foreignkey belirtmek istersem sonuna .HasForeignKey(x => x.CategoryId) diye yazabilirim.
+            builder.HasOne(x => x.Category).WithMany(x => x.Products);
+        }
+    }
+}
